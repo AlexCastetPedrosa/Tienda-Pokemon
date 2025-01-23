2627fb4a413472002285a7e98da1adf8a9877c3e
AlexCastetPedrosa <alexcasteto03@gmail.com>
2025-01-23 16:13:03 +0100

    funcion pokemon tipo seleccionado

--- js/data.js
+++ js/data.js
@@ -3,16 +3,7 @@ const urltypes ='https://pokeapi.co/api/v2/type?limit=18&offset=0';
 /**
  recibe un url y devuelve un objeto con la info de la paginacion y results con los 10 Pokemons de esa pagina
  */
-async function getAllPokemons(url) {
-    try {
-        const peticion = await fetch(url, { method: 'get' })
-        const data = await peticion.json();
-        return data
-    } catch (error) {
-        console.log(error)
-    }
-}
-async function getPokemonInfo(url) {
+async function getApiInfo(url) {
     try {
         const peticion = await fetch(url, { method: 'get' })
         const data = await peticion.json();
--- js/view.js
+++ js/view.js
@@ -1,6 +1,6 @@
 const buttons = document.querySelectorAll('.flex button'); //array de botones
 const sectionGrid = document.querySelector('#characters .grid'); //seccion donde pinto los Pokemons
-const selectStatus = document.querySelector('#statusSelect');
+const selectStatus = document.querySelector('#statusSelect');//select de tipos 
 
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
@@ -10,16 +10,49 @@ function ShinyRoulete(){
     let random = getRandomInt(10);
     return random == 0 ? true : false;
 }
+//añado a los botones el listener
+function goToPage(event) {
+    // aqui necesito saber la pagina de api a la que voy a conectar
+    init(event.target.dataset.url, urltypes)
+}
+
+buttons.forEach(button => button.addEventListener('click', goToPage))
+
+async function obtenerStatus(event) {
+    console.log(event.target.value)
+    const DataType = await getApiInfo(event.target.value);
+    printAllCharactersForType(DataType.pokemon, sectionGrid);
+
+
+    //tendremos que llamar a api para obtener el listado con ese status, y posteriormente pintar
+}
+selectStatus.addEventListener('change', obtenerStatus);
+
+function printAllCharactersForType(list, dom) {
+    dom.innerHTML = "";
+    list.forEach(pokemon => printOneCharacter(pokemon.pokemon, dom))
+    
+}
+
+
+
 
 async function printOneCharacter(pokemon, dom) {
     let shiny = ShinyRoulete();
-    let dataPokemon = await getPokemonInfo(pokemon.url);
+    let dataPokemon = await getApiInfo(pokemon.url);
     const article = document.createElement('article');
     const figure = document.createElement('figure');
     const img = document.createElement('img');
     img.src = shiny ? dataPokemon.sprites.front_shiny : dataPokemon.sprites.front_default;
     img.alt = dataPokemon.name
-    const types = document.createElement('p')   
+    const types = document.createElement('div') 
+        const figureType = document.createElement('figure');
+        const imgType = document.createElement('img');
+        let dataType = await getApiInfo(dataPokemon.types[0].type.url)
+        imgType.src = dataType.sprites['generation-vi']['x-y'].name_icon;
+        imgType.alt = dataType.name;
+    figureType.appendChild(imgType);
+    types.appendChild(figureType);
     const h3 = document.createElement('h3');
     h3.textContent = dataPokemon.name;
 
@@ -27,7 +60,7 @@ async function printOneCharacter(pokemon, dom) {
     description.textContent = 'Descripcion'
 
     figure.appendChild(img)
-    article.append(figure, h3, description);
+    article.append(figure, h3, description, types);
     dom.appendChild(article)
 
 }
@@ -39,7 +72,8 @@ function printAllCharacters(list, dom) {
 }
 function printOneType(type, dom){
     const option = document.createElement('option')
-    option.textContent = type.name
+    option.textContent = type.name;
+    option.value = type.url;
     dom.appendChild(option);
 }
 
@@ -48,8 +82,8 @@ function printAllTypes(types,dom){
 }
 
 async function init(url, urltypes) {
-    const data = await getAllPokemons(url);
-    const dataType = await getAllPokemons(urltypes);
+    const data = await getApiInfo(url);
+    const dataType = await getApiInfo(urltypes);
     buttons[0].dataset.url = data.previous;
     buttons[1].dataset.url = data.next;
     printAllCharacters(data.results, sectionGrid);
